1df976b844b52476604ef275d5d3e4e261486213
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-29 12:16:53 +0300

    insert records

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
-INSERT INTO Customers(customer_id, customer_name, email, address) VALUES (
+INSERT INTO Customer(customer_id, customer_name, email, address) VALUES (
     1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave"
 )
